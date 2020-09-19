c93b3371544a921d13415fc9bf4741c5ec157699
AdiLhan1 <kelsinbekovadilhan2018@gmail.com>
2020-09-19 21:09:32 +0600

    updated

--- app/src/main/java/com/geektech/quizapp_gt_4_2/App.java
+++ app/src/main/java/com/geektech/quizapp_gt_4_2/App.java
@@ -1,5 +1,6 @@
 package com.geektech.quizapp_gt_4_2;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
 import android.util.Log;
@@ -12,19 +13,15 @@ import com.geektech.quizapp_gt_4_2.data.history.HistoryStorage;
 import com.geektech.quizapp_gt_4_2.data.history.IHistoryStorage;
 import com.geektech.quizapp_gt_4_2.data.remote.IQuizApiClient;
 import com.geektech.quizapp_gt_4_2.data.remote.QuizApiClient;
-import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
-import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
-import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
-import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
-import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
 
 public class App extends Application {
     public static IQuizApiClient quizApiClient;
     public static IHistoryStorage historyStorage;
     public static QuizRepository repository;
     public static QuizDatabase quizDatabase;
+    @SuppressLint("StaticFieldLeak")
     public static Context instance;
-    public static FirebaseTranslator myTranslator;
+//    public static FirebaseTranslator myTranslator;
 
     @Override
     public void onCreate() {
@@ -43,33 +40,32 @@ public class App extends Application {
 
         quizApiClient = repository;
         historyStorage = repository;
-        fireBaseBuilder();
 
     }
 
-    private void fireBaseBuilder() {
-        FirebaseTranslatorOptions options =
-                new FirebaseTranslatorOptions.Builder()
-                        .setSourceLanguage(FirebaseTranslateLanguage.EN)
-                        .setTargetLanguage(FirebaseTranslateLanguage.RU)
-                        .build();
-
-        final FirebaseTranslator englishRussianTranslator =
-                FirebaseNaturalLanguage.getInstance().getTranslator(options);
-        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
-                .requireWifi()
-                .build();
-
-        englishRussianTranslator.downloadModelIfNeeded(conditions)
-                .addOnSuccessListener(
-                        v -> {
-                            myTranslator = englishRussianTranslator;
-                            Log.e("TAG", "firebaseBuilder: " + englishRussianTranslator);
-                        })
-                .addOnFailureListener(
-                        e -> {
-                            Log.e("TAG", "firebaseBuilder: " + e.getLocalizedMessage());
-                        });
-
-    }
+//    private void fireBaseBuilder() {
+//        FirebaseTranslatorOptions options =
+//                new FirebaseTranslatorOptions.Builder()
+//                        .setSourceLanguage(FirebaseTranslateLanguage.EN)
+//                        .setTargetLanguage(FirebaseTranslateLanguage.RU)
+//                        .build();
+//
+//        final FirebaseTranslator englishRussianTranslator =
+//                FirebaseNaturalLanguage.getInstance().getTranslator(options);
+//        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
+//                .requireWifi()
+//                .build();
+//
+//        englishRussianTranslator.downloadModelIfNeeded(conditions)
+//                .addOnSuccessListener(
+//                        v -> {
+//                            myTranslator = englishRussianTranslator;
+//                            Log.e("TAG", "firebaseBuilder: " + englishRussianTranslator);
+//                        })
+//                .addOnFailureListener(
+//                        e -> {
+//                            Log.e("TAG", "firebaseBuilder: " + e.getLocalizedMessage());
+//                        });
+//
+//    }
 }
